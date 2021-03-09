46ee6236d03f20cfffc14d8d9c18b261b6dcd1ff
Jennifer Henry <jenhenry1995@gmail.com>
2021-03-09 11:56:49 -0800

    updates

--- TicTacToe/App.js
+++ TicTacToe/App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Title from './components/Title'
+import Title from './components/Title';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Title/>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Title />
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
--- TicTacToe/components/Title.js
+++ TicTacToe/components/Title.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
-export default class Title extends Component{
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text style={styles.title}>Tic Tac Toe</Text>
 
-            </View>
-        )
-    }
+export default class Title extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Tic Tac Toe</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: 300,
-        backgroundColor: 'teal',
-        flexDirection: 'row'
-    },
-    title: {
-        color: 'white',
-        fontSize: 25,
-        textAlign: 'center',
-        flex:1,
-    }
-})
+  container: {
+    width: 300,
+    backgroundColor: 'teal',
+    flexDirection: 'row',
+  },
+  title: {
+    color: 'white',
+    fontSize: 25,
+    textAlign: 'center',
+    flex: 1,
+  },
+});
