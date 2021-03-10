693b162a27237b6edc1c8dd5a2c25613d712f794
Jennifer Henry <jenhenry1995@gmail.com>
2021-03-09 17:22:50 -0800

    switch statement for player change

--- TicTacToe/components/GameBoard.js
+++ TicTacToe/components/GameBoard.js
@@ -39,16 +39,35 @@ export default class GameBoard extends Component {
   renderIcon = (row, col) => {
     var value = this.state.gameState[row][col];
     switch (value) {
+      //player 1
       case 1:
         return <Icon name="close" style={styles.playerX} />;
+      //player 2
       case 2:
         return <Icon name="circle-outline" style={styles.playerO} />;
+      //blanik
       default:
         return <Icon />;
     }
   };
 
-  click = () => {};
+  //player 1 = X ----- player 2 = O ------ default 0 = blank
+  onClick = (row, col) => {
+    var player = this.state.currentPlayer;
+    var array = this.state.gameState.slice();
+
+    //updates gamestate with proper player
+    array[row][col] = player;
+    this.setState({ gameState: array });
+
+    //changes current player
+    switch (player) {
+      case 1:
+        return (this.state.currentPlayer = 2);
+      case 2:
+        return (this.state.currentPlayer = 1);
+    }
+  };
 
   render() {
     return (
@@ -56,19 +75,19 @@ export default class GameBoard extends Component {
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(0, 0)}
+            onPress={() => this.onClick(0, 0)}
           >
             {this.renderIcon(0, 0)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(0, 1)}
+            onPress={() => this.onClick(0, 1)}
           >
             {this.renderIcon(0, 1)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(0, 2)}
+            onPress={() => this.onClick(0, 2)}
           >
             {this.renderIcon(0, 2)}
           </TouchableOpacity>
@@ -77,19 +96,19 @@ export default class GameBoard extends Component {
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(1, 0)}
+            onPress={() => this.onClick(1, 0)}
           >
             {this.renderIcon(1, 0)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(1, 1)}
+            onPress={() => this.onClick(1, 1)}
           >
             {this.renderIcon(1, 1)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(1, 2)}
+            onPress={() => this.onClick(1, 2)}
           >
             {this.renderIcon(1, 2)}
           </TouchableOpacity>
@@ -98,19 +117,19 @@ export default class GameBoard extends Component {
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(2, 0)}
+            onPress={() => this.onClick(2, 0)}
           >
             {this.renderIcon(2, 0)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(2, 1)}
+            onPress={() => this.onClick(2, 1)}
           >
             {this.renderIcon(2, 1)}
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.square}
-            onPress={() => this.click(2, 2)}
+            onPress={() => this.onClick(2, 2)}
           >
             {this.renderIcon(2, 2)}
           </TouchableOpacity>
